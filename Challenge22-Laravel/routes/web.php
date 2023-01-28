ff6cb088377771f8fc2a9c739add2c4abb494c25
Vasil <filipovski.v@hotmail.com>
2023-01-28 03:33:26 +0100

    Added challenge 22

---
+++ Challenge22-Laravel/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('index', [UsersController::class, 'index'])->name('index');
+Route::get('login', [UsersController::class, 'create'])->name('login');
+Route::post('store', [UsersController::class, 'store'])->name('store');
+Route::get('clear', [UsersController::class, 'clear'])->name('clear');
