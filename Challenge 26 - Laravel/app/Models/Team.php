25f64195d669ed87d14938391161a980a0ec7ff9
Vasil <filipovski.v@hotmail.com>
2023-03-16 18:53:35 +0100

    Added challenge 26

---
+++ Challenge 26 - Laravel/app/Models/Team.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    public function player(){
+        return $this->hasMany(Player::class);
+    }
+    public function game(){
+        return $this->hasMany(Game::class);
+    }
+}
---
+++ Challenge 26 - Laravel/database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = 'Admin Jack';
+        $user->email = 'adminjack@admin.com';
+        $user->password = Hash::make('123456');
+        $user->role_id = Role::where('name', 'admin')->first()->id;
+        $user->save();
+
+      
+    }
+}
